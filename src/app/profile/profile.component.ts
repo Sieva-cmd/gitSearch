60318a7c82efaa20aa70ec4443faa657cf73ec83
sieva-cmd <musyokasieva@gmail.com>
2022-02-12 21:02:32 +0300

    update profile information

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,10 @@
+import { environment } from 'src/environments/environment';
 import { ProfileService } from './../profile-service/profile.service';
 import { Component, OnInit } from '@angular/core';
 import { Profile } from '../profile-class/profile';
 import { Repostories } from '../repostories';
 import { HttpClient } from '@angular/common/http';
+
 
 
 
@@ -16,12 +18,16 @@ import { HttpClient } from '@angular/common/http';
 export class ProfileComponent implements OnInit {
 
   profile!:Profile;
-  username! :string;
+  username!:string;
   repos!:Repostories []; 
+  profileName!:string;
+  private clientID = '79b5763e808d746f79ff';
+  private clientSecret = '0ffad460c25332b87fb3be7f0b8389b315ac88d8'
   
 
 
   constructor(private profileService: ProfileService,private http:HttpClient) {
+    this.profileName = '/repos'
    
    }
 
@@ -29,19 +35,17 @@ export class ProfileComponent implements OnInit {
     this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo()
     this.profile = this.profileService.profile
+    this.getRepoInfo();
   }
 
   ngOnInit() {
     
- 
 
-    this.getRepoInfo();
 
-   
   }
 
     getRepoInfo(){
-      this.http.get<any>("https://api.github.com/users/Sieva-cmd/repos").subscribe(
+      this.http.get<any>(environment.repoUrl).subscribe(
         response =>{
            console.log(response);
           this.repos = response;
